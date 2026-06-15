f4c8fbce5ca00cd074abf1ca8933ad6151ab0631
Ole Wold <oleh.wold@gmail.com>
2026-06-15 22:20:43 +0200

    Chore: legg til .gitignore for temp/QA-filer og rydd ut

--- .astro-stale-2/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/emner/nettverk/osi-modellen-interaktiv.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Femner%2Fnettverk%2Fosi-modellen-interaktiv.mdx&astroContentModuleFlag=true")]]);
-		
--- check_yaml.py
+++
@@ -1,3 +0,0 @@
-import yaml, sys
-yaml.safe_load(open(sys.argv[1]))
-print("YAML valid")
